21548d762065547e1b2255a03e949ff3c3e36471
Mario Mora <marioemorac@gmail.com>
2024-02-11 21:59:51 +0100

    Eliminando referencias a NgxSpinner
    Eliminando referencias a NgxSpinner

--- src/app/componentes/ensenanzas/ensenanzas.component.ts
+++ src/app/componentes/ensenanzas/ensenanzas.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
-import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-ensenanzas',
