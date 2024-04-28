21a1c2247fc61b77835b49ce142b26efec281e1b
Mario Mora <marioemorac@gmail.com>
2024-04-28 08:39:35 +0200

    Actualizando navbar
    Se agregó al navbar la funcionalidad de colapsar el menú en dispositivo móvil

--- src/app/componentes/ministerios/ministerios.component.spec.ts
+++ src/app/componentes/ministerios/ministerios.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { MinisteriosComponent } from './ministerios.component';
+import { of } from 'rxjs';
 
 describe('MinisteriosComponent', () => {
   let component: MinisteriosComponent;
@@ -8,15 +9,30 @@ describe('MinisteriosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MinisteriosComponent ]
+      declarations: [ MinisteriosComponent ],
+      providers: [
+        // Proveedor simulado de ActivatedRoute
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({ ministerio: 'hombres' }) // Simula un parámetro en la ruta
+            },
+            params: of({ ministerio: 'hombres' }) // Simula los parámetros observables
+          }
+        }
+      ]
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(MinisteriosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    .compileComponents()
   });
 
+  beforeEach(() => {
+      fixture = TestBed.createComponent(MinisteriosComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+  });    
+
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
