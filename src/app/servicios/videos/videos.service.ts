fd5f1851b451514d896cb0dc0e4363f7e734e6c9
Mario Mora <marioemorac@gmail.com>
2024-05-16 12:40:01 +0200

    Agregando Ver Sermón

--- src/app/servicios/videos/videos.service.ts
+++ src/app/servicios/videos/videos.service.ts
@@ -6,7 +6,7 @@ import { video } from '../../modelos/videos';
 })
 export class VideosService {
 
-  videos:video[] = [new video("¿Como habló Dios con Jacob puede también Dios hablar conmigo? - Génesis 31:1-21", "https://www.youtube.com/embed/ujL1F4zhUR4?si=kyP9NdRQPF9gSlDe", "11 de febrero"),
+  videos:video[] = [new video("El proposito pricipial del libro de Rut", "https://www.youtube.com/embed/pDFX3G146U4?si=LPkIl79mrSj_c8ZK", "27 de noviembre de 2022"),
                     new video("1 Corintios 10 - el ejemplo de los Judíos", "https://www.youtube.com/embed/OKCFBEPNyvU?si=tCxS23hkwNbkZt0v", "8 de febrero"),
                     new video("Seminario ( introducción )", "https://www.youtube.com/embed/Nj_4Q8cBoxc?si=P0TxTB5jE2BT6Rxq", "5 de febrero")
 ];
