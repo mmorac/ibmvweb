43a207895d1352ae9cdd06d7eb6a2af83424d1f2
Mario Mora <marioemorac@gmail.com>
2024-07-03 17:40:11 +0200

    Actualización para publicación

--- src/app/servicios/videos/videos.service.ts
+++ src/app/servicios/videos/videos.service.ts
@@ -7,7 +7,7 @@ import { video } from '../../modelos/videos';
 export class VideosService {
 
   videos:video[] = [new video("El proposito pricipial del libro de Rut", "https://www.youtube.com/embed/pDFX3G146U4?si=LPkIl79mrSj_c8ZK", "27 de noviembre de 2022"),
-                    new video("1 Corintios 10 - el ejemplo de los Judíos", "https://www.youtube.com/embed/OKCFBEPNyvU?si=tCxS23hkwNbkZt0v", "8 de febrero"),
+                    new video("De que sirve el Cristianismo que no te cuesta nada 1 Samuel 2", "https://www.youtube.com/embed/VTGU21wompo?si=6Cyfb-U0yd5q2HJc", "30 de junio de 2024"),
                     new video("Seminario ( introducción )", "https://www.youtube.com/embed/Nj_4Q8cBoxc?si=P0TxTB5jE2BT6Rxq", "5 de febrero")
 ];
   constructor() { }
